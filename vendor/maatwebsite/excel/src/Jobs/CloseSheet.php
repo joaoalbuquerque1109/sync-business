8a66e8ee96452da968726707c32e5722affa2a70
joaoalbuquerque1109 <163339676+joaoalbuquerque1109@users.noreply.github.com>
2026-03-20 12:48:22 -0300

    first commit

---
+++ vendor/maatwebsite/excel/src/Jobs/CloseSheet.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Maatwebsite\Excel\Jobs;
+
+use Illuminate\Bus\Batchable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Files\TemporaryFile;
+use Maatwebsite\Excel\Writer;
+
+class CloseSheet implements ShouldQueue
+{
+    use Batchable, Dispatchable, InteractsWithQueue, ProxyFailures, Queueable;
+
+    /**
+     * @var object
+     */
+    private $sheetExport;
+
+    /**
+     * @var string
+     */
+    private $temporaryFile;
+
+    /**
+     * @var string
+     */
+    private $writerType;
+
+    /**
+     * @var int
+     */
+    private $sheetIndex;
+
+    /**
+     * @param  object  $sheetExport
+     * @param  TemporaryFile  $temporaryFile
+     * @param  string  $writerType
+     * @param  int  $sheetIndex
+     */
+    public function __construct($sheetExport, TemporaryFile $temporaryFile, string $writerType, int $sheetIndex)
+    {
+        $this->sheetExport   = $sheetExport;
+        $this->temporaryFile = $temporaryFile;
+        $this->writerType    = $writerType;
+        $this->sheetIndex    = $sheetIndex;
+    }
+
+    /**
+     * @param  Writer  $writer
+     *
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     * @throws \PhpOffice\PhpSpreadsheet\Reader\Exception
+     */
+    public function handle(Writer $writer)
+    {
+        // Determine if the batch has been cancelled...
+        if ($this->batch()?->cancelled()) {
+            return;
+        }
+
+        $writer = $writer->reopen(
+            $this->temporaryFile,
+            $this->writerType
+        );
+
+        $sheet = $writer->getSheetByIndex($this->sheetIndex);
+
+        if ($this->sheetExport instanceof WithEvents) {
+            $sheet->registerListeners($this->sheetExport->registerEvents());
+        }
+
+        $sheet->close($this->sheetExport);
+
+        $writer->write(
+            $this->sheetExport,
+            $this->temporaryFile,
+            $this->writerType
+        );
+    }
+}
---
+++ vendor/maatwebsite/excel/src/Jobs/StoreQueuedExport.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Maatwebsite\Excel\Jobs;
+
+use Illuminate\Bus\Batchable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Maatwebsite\Excel\Files\Filesystem;
+use Maatwebsite\Excel\Files\TemporaryFile;
+
+class StoreQueuedExport implements ShouldQueue
+{
+    use Batchable, Dispatchable, InteractsWithQueue, Queueable;
+
+    /**
+     * @var string
+     */
+    private $filePath;
+
+    /**
+     * @var string|null
+     */
+    private $disk;
+
+    /**
+     * @var TemporaryFile
+     */
+    private $temporaryFile;
+
+    /**
+     * @var array|string
+     */
+    private $diskOptions;
+
+    /**
+     * @param  TemporaryFile  $temporaryFile
+     * @param  string  $filePath
+     * @param  string|null  $disk
+     * @param  array|string  $diskOptions
+     */
+    public function __construct(TemporaryFile $temporaryFile, string $filePath, ?string $disk = null, $diskOptions = [])
+    {
+        $this->disk          = $disk;
+        $this->filePath      = $filePath;
+        $this->temporaryFile = $temporaryFile;
+        $this->diskOptions   = $diskOptions;
+    }
+
+    /**
+     * @param  Filesystem  $filesystem
+     */
+    public function handle(Filesystem $filesystem)
+    {
+        // Determine if the batch has been cancelled...
+        if ($this->batch()?->cancelled()) {
+            return;
+        }
+
+        $filesystem->disk($this->disk, $this->diskOptions)->copy(
+            $this->temporaryFile,
+            $this->filePath
+        );
+
+        $this->temporaryFile->delete();
+    }
+}
